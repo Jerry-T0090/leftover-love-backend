43d1544bf323d55d40a57b2b18cf9f551d5426bd
zaina-qasim <zaina-qasim@blueinnovationsgroup.com>
2023-09-17 10:49:16 -0400

    Added CORS

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, make_response
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from database import perform_db_operation
 
 app = Flask(__name__)
 api = Api(app)
-
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 class Recipient(Resource):
     
     def post(self):
